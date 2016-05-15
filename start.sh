bbdd03e42baf630e9decb0c08c5b72063dcb5c23
Sveinn Svavarsson <sveinn@greenqloud.com>
2016-05-15 14:30:50 +0000

    Working version of django on top of python 3.4

--- start.sh
+++ start.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-apt-get update && apt-get -y upgrade
-apt-get -y install apache2 python-pip libapache2-mod-wsgi
-apt-get install debconf-utils
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y install mysql-server-5.5 --fix-missing
-apt-get -y install virtualenv --fix-missing
+apt-get update && apt-get -y upgrade
+apt-get -y install apache2 python3-pip libapache2-mod-wsgi-py3 mysql-server-5.5 virtualenv python3-virtualenv
+update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+update-alternatives --install /usr/bin/python python /usr/bin/python3.4 2
 cd /opt/
-virtualenv django-env
+virtualenv -p /usr/bin/python django-env
 cd django-env
 . bin/activate
 pip install django
 django-admin startproject kkidb
 cat > /etc/apache2/conf-available/kkidb.conf <<EOF
 WSGIScriptAlias / /opt/django-env/kkidb/kkidb/wsgi.py
-WSGIPythonPath /opt/django-env/kkidb:/opt/django-env/lib/python2.7/site-packages
+WSGIPythonPath /opt/django-env/kkidb:/opt/django-env/lib/python3.4/site-packages
 
 <Directory /opt/django-env/kkidb>
 <Files wsgi.py>
